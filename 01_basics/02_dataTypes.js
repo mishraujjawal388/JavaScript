294a1549602c195993b1c6d2a793fcab6dbf641f
Ujjawal Mishra <79258995+mishraujjawal388@users.noreply.github.com>
2024-01-26 07:13:38 +0000

    data type done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,41 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+/*
+we can not use alert in this methode 
+because its a node js not browser
+//alert is not defined
+*/
+
+// console.log(3+3); console.log("ujjawal"); //we can not use this type its working but this is not a better way to write code beause its not a readable or proper code writing
+
+console.log(3+3)
+console.log("ujjawal")
+
+// task read https://tc39.es/ecma262/#sec-intro
+
+let name = "shubham"
+let age = 23
+let isLoggedIn = false
+let state;
+
+/*
+data types
+
+1. premetive data types
+
+    // number => 2 to power 53
+    // biginit
+    // string => ""
+    // boolean => true/false
+    // null => standlone value (representation of empty value)
+    // undefined => 
+    // symbol => unique
+
+2. non premitive data types
+    // oject
+*/
+
+console.log(typeof null); //is a object data types
+console.log(typeof undefined); //is a undefined data types
